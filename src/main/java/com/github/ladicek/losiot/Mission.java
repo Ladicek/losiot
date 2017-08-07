a5597f24f4064b197648bf5449278493399a2c6e
Ladislav Thon <lthon@redhat.com>
2017-08-07 17:18:31 +0200

    add text for the ConfigMap mission

--- src/main/java/com/github/ladicek/losiot/Mission.java
+++ src/main/java/com/github/ladicek/losiot/Mission.java
@@ -3,7 +3,7 @@ package com.github.ladicek.losiot;
 public enum Mission {
     M100_HTTP_API("REST API Level 0"),
     M101_CRUD(null),
-    M102_CONFIGMAP(null),
+    M102_CONFIGMAP("Externalized Configuration"),
     M103_SSO(null),
     M104_HEALTH_CHECK("Health Check"),
     ;
